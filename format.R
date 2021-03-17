b04402074c26d3a6fb00c9550e30fd0048f63204
rwmcelr <75845442+rwmcelr@users.noreply.github.com>
2021-03-17 16:18:18 -0400

    Add files via upload

---
+++ format.R
@@ -0,0 +1,26 @@
+## Library calls/ set script variables -------------------------------------
+setwd("/Users/rmcelroy/Desktop/PEA/")
+library("clusterProfiler")
+
+WtMut <- read.csv("WtMut_PathwayData.csv")
+wtMut <- WtMut[,c("entrez","log2FoldChange","pvalue")]
+wtMut <- wtMut[complete.cases(wtMut), ]
+wtMut <- wtMut[order(-wtMut$log2FoldChange),]
+
+wtMut_geneList <- wtMut$log2FoldChange
+names(wtMut_geneList) <- wtMut$entrez
+
+wtMut_sig <- subset(wtMut, pvalue < 0.05)
+wtMut_genes <- wtMut_sig$log2FoldChange
+names(wtMut_genes) <- wtMut_sig$entrez
+genes <- names(wtMut_genes)
+
+go_enrich <- enrichGO(gene = genes,
+                      universe = names(wtMut_geneList),
+                      OrgDb = "org.Hs.eg.db",
+                      keyType = 'ENTREZID',
+                      readable = T,
+                      ont = "BP",
+                      pvalueCutoff = 0.05)
+dotplot(go_enrich)
+goplot(go_enrich, showCategory = 10)
